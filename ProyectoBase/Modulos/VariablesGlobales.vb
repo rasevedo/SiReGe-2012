efdb50071aa6e7748d8a652d1eb7d40aa87924a7
Rasevedo <rasevedo9@gmail.com>
2020-12-18 10:00:14 -0600

    Commit del todo el sistema version 2012

---
+++ ProyectoBase/Modulos/VariablesGlobales.vb
@@ -0,0 +1,14 @@
+﻿Module VariablesGlobales
+
+    Public ConexionSQLServer As New SqlClient.SqlConnection
+
+    Public _Funciones As New CapLogicaNegocioSiReGe.Funciones
+
+    Public _WSIntegra2 As New CapLogicaNegocioSiReGe.Integra2.WebIntegra
+    Public _WSSigrh As New CapLogicaNegocioSiReGe.SIGRH.WebSigrh
+
+    Enum BaseDatos
+        NombreBaseDatos = 1
+    End Enum
+
+End Module
